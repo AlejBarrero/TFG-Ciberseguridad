84f74b45b1084dc1b6d13351b6bd8dac9a114270
Alejandro Barrero <krote95@gmail.com>
2026-07-12 23:59:45 +0200

    Add DevSecOps security workflows

--- config/config.php
+++ config/config.php
@@ -2,12 +2,11 @@
 
 define("APP_NAME","SecureDesk");
 
-define("APP_URL","http://localhost/SecureDesk");
+define("APP_URL","https://alejbarrero-tgf-ciberseguridad.page.gd");
 
-define("DB_HOST","localhost");
-define("DB_NAME","nombre_base_datos");
-define("DB_USER","usuario_mysql");
-define("DB_PASS","password_mysql");
+define("DB_HOST","sql308.infinityfree.com");
+define("DB_NAME","if0_42395487_tfgciberseguridad");
+define("DB_USER","if0_42395487");
+define("DB_PASS","Vd1QSm6CzC6chVM");
 
 ?>
-
